cc152e9701611529559eb71ab1471d3dd772cbe5
Netto Farah <nettofarah@gmail.com>
2020-06-25 11:07:52 -0500

    Add Scope & CI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testRunner: '@undefinedlabs/scope-agent/jest/testRunner',
+  setupFilesAfterEnv: ['@undefinedlabs/scope-agent/jest/setupTests'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   globalSetup: '<rootDir>/jest-setup.ts',
   clearMocks: true,
